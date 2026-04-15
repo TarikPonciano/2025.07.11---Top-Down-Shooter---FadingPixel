94d37edde32dc3e93e93cda162d068a358a69680
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-04-15 16:30:17 -0300

    Código de tiro

--- scripts/jogador.gd
+++ scripts/jogador.gd
@@ -6,14 +6,21 @@ const JUMP_VELOCITY = -400.0
 # Direction vai se referir ao movimento do jogador
 var direction = Vector2(0,0)
 
+@export var projetil: PackedScene
 
 func _physics_process(delta: float) -> void:
 	
 	rotacionar_corpo()
 	mover()
 	
-	
 	move_and_slide()
+	
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("atirar"):
+		disparar()
+		
+func disparar():
+	pass
 
 func mover():
 	
