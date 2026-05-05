fbbc20e6474c54e943ccb529869ecd0c7edacc57
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-05-05 16:38:03 -0300

    Inimigo

--- scripts/fase_1.gd
+++ scripts/fase_1.gd
@@ -3,23 +3,34 @@ extends Node2D
 @export var inimigo_cena: PackedScene
 
 func spawn_inimigo():
+	#Cria uma cópia do inimigo(Cena)
 	var novo_inimigo = inimigo_cena.instantiate()
 	
+	# Gera a posição que o inimigo deverá spawnar
+	
+	# Distancia horizontal em relação ao jogador
 	var rand_posX = randi_range(-400, 400)
+	# Distancia vertical em relação ao jogador 
 	var rand_posY = randi_range(-250, 250)
+	
+	# Cálculo da posição que ele vai aparecer no jogo
 	var posX = %Jogador.global_position.x + rand_posX
 	var posY = %Jogador.global_position.y + rand_posY
 	
+	# Verificação se a posição gerada foi válida
 	while true:
+		# Verificar se o inimigo está longe o suficiente do jogador
 		if %Jogador.global_position.distance_to(Vector2(posX, posY)) > 100:
 			break
-			
+		
+		# Verificar se o inimigo está spawnando dentro da fase verticalmente
 		if posY >= 0 and posY <= 648:
 			break
-			
+		# Verificar se o inimigo está spawnando dentro da fase horizontalmente
 		if posX >= 0 and posX <= 1152:
 			break
 			
+		#Gera todas as informações de posição de novo
 		rand_posX = randi_range(-400, 400)
 		rand_posY = randi_range(-250, 250)
 		posX = %Jogador.global_position.x + rand_posX
